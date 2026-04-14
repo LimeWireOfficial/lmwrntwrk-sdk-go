5626b9f67142a3618fc9d7368918e6ead5f8e148
haris <haris@limewire.com>
2026-04-14 15:22:10 +0200

    chore: release v0.1.4 - update SDK version

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 import "fmt"
 
 // SDKVersion is the current version of the LimeWireNetwork SDK for Go.
-var SDKVersion = "0.1.3"
+var SDKVersion = "0.1.4"
 
 // UserAgent is the pre-calculated User-Agent string for the SDK.
 func UserAgent() string {
